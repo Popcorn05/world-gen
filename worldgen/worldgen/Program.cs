7422dcc689446dc9f5aa209ca199b27442f89a36
Popcorn05 <43976660+Popcorn05@users.noreply.github.com>
2021-05-03 12:40:21 +1000

    Implemented Example Code For Testing

--- worldgen/worldgen/Program.cs
+++ worldgen/worldgen/Program.cs
@@ -6,7 +6,22 @@ namespace worldgen
     {
         static void Main(string[] args)
         {
+            // Create and configure FastNoise object
             FastNoiseLite noise = new FastNoiseLite();
+            noise.SetNoiseType(FastNoiseLite.NoiseType.OpenSimplex2);
+
+            // Gather noise data
+            float[] noiseData = new float[128 * 128];
+            int index = 0;
+
+            for (int y = 0; y < 128; y++)
+            {
+                for (int x = 0; x < 128; x++)
+                {
+                    noiseData[index] = noise.GetNoise(x, y);
+                    Console.WriteLine(noiseData[index++]);
+                }
+            }
         }
     }
 }
