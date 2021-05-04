3dadb4596d5a2d8c6b3d1db8d80e072d84daed2b
Popcorn05 <43976660+Popcorn05@users.noreply.github.com>
2021-05-04 15:09:08 +1000

    Displaying Raw Perlin Noise
    Using Raylib to convert data into an image format first then into texture, then drawing to screen. No generation code yet.

--- worldgen/worldgen/Generation.cs
+++ worldgen/worldgen/Generation.cs
@@ -2,6 +2,12 @@ namespace worldgen
 {
     public class Generation
     {
-        //Generation methods
+        // Useful Math methods
+        
+        public static double map(double x, double in_min, double in_max, double out_min, double out_max) {
+            return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+        }
+        
+        // Generation methods
     }
 }
--- worldgen/worldgen/Program.cs
+++ worldgen/worldgen/Program.cs
@@ -1,26 +1,97 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using Raylib_cs;
+using static Raylib_cs.Raylib;
+using static Raylib_cs.PixelFormat;
+using static worldgen.Generation;
 
 namespace worldgen
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
+            //Control variables
+
+            const int GENWIDTH = 1024;
+            const int GENHEIGHT = 1024;
+            
+            // Window setup
+            
+            InitWindow(GENWIDTH,GENHEIGHT,"Knallkorn's World Generator");
+
             // Create and configure FastNoise object
             FastNoiseLite noise = new FastNoiseLite();
             noise.SetNoiseType(FastNoiseLite.NoiseType.Perlin);
 
             // Gather noise data
-            float[] noiseData = new float[1024 * 1024];
+            int[] noiseData = new int[GENWIDTH * GENHEIGHT];
             int index = 0;
 
-            for (int y = 0; y < 128; y++)
+            for (int y = 0; y < GENHEIGHT; y++)
             {
-                for (int x = 0; x < 128; x++)
+                for (int x = 0; x < GENWIDTH; x++)
                 {
-                    noiseData[index] = noise.GetNoise(x, y);
+                    noiseData[index++] = Convert.ToInt32(map(noise.GetNoise(x, y),-1.0,1.0,0.0,255.0));
                 }
             }
+            
+            // Create colour array
+
+            Color[] displayColors = new Color[GENWIDTH * GENHEIGHT];
+            index = 0;
+            
+            for (int y = 0; y < GENHEIGHT; y++)
+            {
+                for (int x = 0; x < GENWIDTH; x++)
+                {
+                    displayColors[y * GENWIDTH + x] = new Color(noiseData[index],noiseData[index],noiseData[index++],255);
+                }
+            }
+
+            // Convert to texture
+            
+            GCHandle pinnedArray = GCHandle.Alloc(displayColors, GCHandleType.Pinned);
+            IntPtr displayColorsPtr = pinnedArray.AddrOfPinnedObject();
+
+            Image displayImg = new Image
+            {
+                data = displayColorsPtr,
+                width = GENWIDTH,
+                height = GENHEIGHT,
+                format = PixelFormat.UNCOMPRESSED_R8G8B8A8,
+                mipmaps = 1,
+            };
+
+            Texture2D displayTex = LoadTextureFromImage(displayImg);
+            pinnedArray.Free();
+
+            //Main loop
+
+            while (!WindowShouldClose())
+            {
+                // Update-------------
+                
+                
+                
+                // Draw---------------
+                
+                BeginDrawing();
+                ClearBackground(Color.RAYWHITE);
+                
+                DrawTexture(displayTex,0,0,Color.WHITE);
+                
+                EndDrawing();
+                
+            }
+            
+            UnloadTexture(displayTex);
+            
+            CloseWindow();
+
+            return 0;
+
         }
     }
 }
