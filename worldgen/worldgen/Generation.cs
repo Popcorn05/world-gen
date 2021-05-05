c5962d94daa9b57d074dff4ae989ad9346e65a79
Popcorn05 <43976660+Popcorn05@users.noreply.github.com>
2021-05-05 12:41:20 +1000

    Implemented  Octaves, Persistance and Lacunarity
    Added GetNoiseOctaves Method

--- worldgen/worldgen/Generation.cs
+++ worldgen/worldgen/Generation.cs
@@ -8,6 +8,26 @@ namespace worldgen
             return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
         }
         
+        // Noise functions
+
+        public static double GetNoiseOctave(ref FastNoiseLite noise, float x, float y, int o, float p, float l)
+        {
+            double total = 0;
+            float freq = 1;
+            double amp = 1;
+            double max = 0;
+            
+            for (int i = 0; i < o; i++)
+            {
+                total += noise.GetNoise(x * freq, y * freq) * amp;
+                max += amp;
+                amp *= p;
+                freq *= l;
+            }
+
+            return total / max;
+        }
+        
         // Generation methods
     }
 }
--- worldgen/worldgen/Program.cs
+++ worldgen/worldgen/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using Raylib_cs;
@@ -18,8 +19,11 @@ namespace worldgen
             const int GENWIDTH = 1024;
             const int GENHEIGHT = 1024;
 
-            const int SCALE = 3;
-            
+            const float SCALE = 1.0f;
+            const int OCTAVES = 2;
+            const float PERSISTANCE = 1.0f;
+            const float LACUNARITY = 2.0f;
+
             // Window setup
             
             InitWindow(GENWIDTH,GENHEIGHT,"Knallkorn's World Generator");
@@ -29,27 +33,31 @@ namespace worldgen
             noise.SetNoiseType(FastNoiseLite.NoiseType.Perlin);
 
             // Gather noise data
-            int[] noiseData = new int[GENWIDTH * GENHEIGHT];
+            double[] noiseData = new double[GENWIDTH * GENHEIGHT];
             int index = 0;
 
+            index = 0;
             for (int y = 0; y < GENHEIGHT; y++)
             {
                 for (int x = 0; x < GENWIDTH; x++)
                 {
-                    noiseData[index++] = Convert.ToInt32(map(noise.GetNoise(x/SCALE, y/SCALE),-1.0,1.0,0.0,255.0));
+                    noiseData[index++] = GetNoiseOctave(ref noise, x / SCALE, y / SCALE, OCTAVES, PERSISTANCE, LACUNARITY);
                 }
             }
-            
+
             // Create colour array
 
             Color[] displayColors = new Color[GENWIDTH * GENHEIGHT];
             index = 0;
+            double minVal = noiseData.Min();
+            double maxVal = noiseData.Max();
             
             for (int y = 0; y < GENHEIGHT; y++)
             {
                 for (int x = 0; x < GENWIDTH; x++)
                 {
-                    displayColors[y * GENWIDTH + x] = new Color(noiseData[index],noiseData[index],noiseData[index++],255);
+                    int val = Convert.ToInt32(Math.Round(map(noiseData[index++],minVal,maxVal,0.0,255.0)));
+                    displayColors[y * GENWIDTH + x] = new Color(val,val,val,255);
                 }
             }
 
